1c364f08ddd15b5aa3b8ac0891296e077026eb2e
Kamlesh Patel <kamlesh.patel18@gmail.com>
2026-09-05 11:51:51 -0400

    kamlesh1808 github setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,13 @@ export default defineNuxtConfig({
         { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css' },
         { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css' }
       ]
-    }
-  },
+    },
+      // Replace with your exact GitHub repository name, keeping the slashes
+      baseURL: '/kamlesh1808/',
+      nitro: {
+        preset: 'github-pages'
+      }
+    },
   typescript: { strict: true }
 })
+
